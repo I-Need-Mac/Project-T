4ade1bda2006050cd73448d0371475d9123b1620
fist-99 <aveleedoo@gmail.com>
2023-03-01 18:07:56 +0900

    feat : StoryManager 분리

--- Project-T/Assets/Scripts/InGame/GameManager.cs
+++ Project-T/Assets/Scripts/InGame/GameManager.cs
@@ -8,20 +8,11 @@ public class GameManager : MonoBehaviour
     public TMP_Text StoryText;
     public TMP_Text[] ChoiceText;
 
-    Dictionary<string, Dictionary<string, object>> story_load;
-    Dictionary<string, Dictionary<string, object>> story;
-    Dictionary<string, Dictionary<string, object>> choice;
+    public string curentStoryID;
 
     // Start is called before the first frame update
     void Start()
     {
-        story_load = CSVReader.Read("Story_load");
-        DebugManager.Instance.PrintDebug("story_load");
-
-        story = CSVReader.Read("Story");
-        DebugManager.Instance.PrintDebug("story");
-        
-        choice = CSVReader.Read("Choice");
         DebugManager.Instance.PrintDebug("choice");
 
         StoryUpdate("RYTA1001");
@@ -34,11 +25,10 @@ public class GameManager : MonoBehaviour
         
     }
 
-    //스토리 매니저에 들어갈 부분
     void StoryUpdate(string storyID)
     {
-        string storyT = (string)story[storyID]["story"];
-        string choice_ID = (string)story_load[storyID]["choice_group_ID"];
+        string storyT = (string)StoryManager.Instance.getStory(storyID)["story"];
+        string choiceID = (string)StoryManager.Instance.getStory_load(storyID)["choice_group_ID"];
 
         DebugManager.Instance.PrintDebug(storyT);
 
@@ -48,26 +38,30 @@ public class GameManager : MonoBehaviour
 
         StoryChange(storyT);
 
-        string choice_1 = choice.ContainsKey(choice_ID + "1") ? (string)choice[choice_ID + "1"]["choice_text_ID"] : "";
-        string choice_2 = choice.ContainsKey(choice_ID + "2") ? (string)choice[choice_ID + "2"]["choice_text_ID"] : "";
-        string choice_3 = choice.ContainsKey(choice_ID + "3") ? (string)choice[choice_ID + "3"]["choice_text_ID"] : "";
-        string choice_4 = choice.ContainsKey(choice_ID + "4") ? (string)choice[choice_ID + "4"]["choice_text_ID"] : "";
 
+        Dictionary<string, object>[] choices = StoryManager.Instance.getChoice(choiceID);
 
-        ChoiceChange(choice_1, choice_2, choice_3, choice_4);
+        string[] choice = new string[choices.Length];
+        for(int i = 0; i < choices.Length; i++)
+        {
+            choice[i] = (string)choices[i]["choice_text_ID"];
+        }
+
+        ChoiceChange(choice);
     }
+
     //UI 매니저에 들어갈 부분
     public void StoryChange(string story)
     {
         StoryText.text = story;
     }
-    public void ChoiceChange(string choice_1, string choice_2, string choice_3, string choice_4)
+    public void ChoiceChange(string[] choice)
     {
         // 나눠주는거 구현들어오는거 판단해서 
-        ChoiceText[0].text = choice_1;
-        ChoiceText[1].text = choice_2;
-        ChoiceText[2].text = choice_3;
-        ChoiceText[3].text = choice_4;
+        for (int i = 0; i < choice.Length; i++)
+        {
+            ChoiceText[i].text = choice[i];
+        }
     }
 
 }
--- Project-T/Assets/Scripts/InGame/StoryManager.cs
+++ Project-T/Assets/Scripts/InGame/StoryManager.cs
@@ -2,7 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoryManager : MonoBehaviour
+public class StoryManager : SingleTon<StoryManager>
 {
-    
+    private Dictionary<string, Dictionary<string, object>> story_load;
+    private Dictionary<string, Dictionary<string, object>> story;
+    private Dictionary<string, Dictionary<string, object>> choice;
+
+    public StoryManager()
+    {
+        SetStoryManager();
+    }
+
+    public void SetStoryManager() {
+        SetCSVRead();
+    }
+
+    public void SetCSVRead()
+    {
+        story_load = CSVReader.Read("Story_load");
+        DebugManager.Instance.PrintDebug("story_load");
+
+        story = CSVReader.Read("Story");
+        DebugManager.Instance.PrintDebug("story");
+
+        choice = CSVReader.Read("Choice");
+        DebugManager.Instance.PrintDebug("choice");
+    }
+
+    public Dictionary<string, object> getStory_load(string storyID)
+    {
+        return story_load[storyID];
+    }
+
+    public Dictionary<string, object> getStory(string storyID)
+    {
+        return story[storyID];
+    }
+    public Dictionary<string, object>[] getChoice(string choiceID)
+    {
+        int countChoice = 0;
+        for(int i = 1; i <= 4; i++)
+        {
+            if(choice.ContainsKey(choiceID + i))
+            {
+                countChoice++;
+            }
+        }
+
+        Dictionary<string, object>[] Choices = new Dictionary<string, object>[countChoice];
+        for(int i = 1; i <= countChoice; i++)
+        {
+            Choices[i - 1] = choice[choiceID + i];
+        }
+
+        return Choices;
+    }
+
+
 }
